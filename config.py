c487cac384679fe7d53377cf827fb53aecb4e54a
Никифоров Сергей Игоревич <tat100niksi@tattelecom.ttc>
2024-07-03 16:15:54 +0300

    config

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "7197357794:AAHi0eXSs-eo0vTh17Ma9SIV6FuVjItQQSQ"
+db_name = 'QSR'
+user = 'tat100yakvv'
+password = 'qwer1234'
+host = "10.19.63.2"
+port = 4432
+
+db_name_cpu = 'zabbix'
+user_cpu = 'read4qlik'
+password_cpu = 'EVD~5mJk'
+host_cpu = "192.168.114.164"
+port_cpu = 5432
